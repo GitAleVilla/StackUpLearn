c25093017db94463eb194fbd39ccce63eea83777
GitAleVilla <villaalessandro16@gmail.com>
2024-08-13 17:30:33 +0200

    Data Science Guessing Game 1.2

--- GuessingGame/guessingGame.py
+++ GuessingGame/guessingGame.py
@@ -2,9 +2,12 @@ import random
 
 def guess_number():
     rand_number = random.randint(1,20)
+    list_of_guesses = []
 
     while True:
         guess = int(input("Guess a number between 1 to 20:"))
+        list_of_guesses.append(guess)
+
         if rand_number < guess:
             print("The guess is too high!")
         elif rand_number > guess:
@@ -12,5 +15,8 @@ def guess_number():
         else:
             print("You got it!")
             break
+
+    print("You guessed the following numbers: ", list_of_guesses)
+
 
 guess_number()
